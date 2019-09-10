fadf24efe327964c941cd4d4fb5b1e36ede156c9
Cristian Nieto <nieto.cristian.alberto@gmail.com>
2019-09-10 01:04:58 -0300

    Added check for permission on Backup and Restore

--- NuSmart.BLL/BLLBackupManager.cs
+++ NuSmart.BLL/BLLBackupManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NuSmart.DAL;
+using NuSmart.BE;
 
 namespace NuSmart.BLL
 {
@@ -11,11 +12,13 @@ namespace NuSmart.BLL
     {
         public int crearBackup(string path)
         {
+            Sesion.Instancia().verificarPermiso("AA099");
             DALBackupManager dalBackupManager = new DALBackupManager();
             return dalBackupManager.crearBackup(path);
         }
         public int ejecutarRestore(string path)
         {
+            Sesion.Instancia().verificarPermiso("AA099");
             DALBackupManager dalBackupManager = new DALBackupManager();
             return dalBackupManager.ejecutarRestore(path);
         }
