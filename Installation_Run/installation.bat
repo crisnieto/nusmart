72ee0dab29a01eb7f197553fce8d511341e965e8
Cristian Nieto <nieto.cristian.alberto@gmail.com>
2019-11-12 13:30:44 -0300

    Added installation stuff

--- Installation_Run/installation.bat
+++ Installation_Run/installation.bat
@@ -6,6 +6,8 @@ echo Please introduce your Database Engine Server Name, example: DESKTOP-EJGFO76
 
 echo ......................................................................................
 
+rm test.txt
+
 set /P name=Enter Database Engine Server Name:
 sqlcmd -S %name% -i installation.sql
 
